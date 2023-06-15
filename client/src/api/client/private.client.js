05013974a886426e3cae7a4771c6973d3c282b69
Roman Kalistratov <80212286+roman-kalistratov@users.noreply.github.com>
2023-06-15 22:24:50 +0300

    changed baseURL

--- client/src/api/client/private.client.js
+++ client/src/api/client/private.client.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import queryString from "query-string";
 
-const baseURL = "http://localhost:5000/api/v1/"; //server URL
+const baseURL = "https://rk-shop.vercel.app/api/v1/"; //server URL
 const privateClient = axios.create({
   baseURL,
   paramsSerializer: {
