a56504d5c8bcc9350932755001964f920de2fa43
Roman Kalistratov <80212286+roman-kalistratov@users.noreply.github.com>
2023-07-29 17:34:28 +0300

    update mail-service

--- server/service/mail-service.js
+++ server/service/mail-service.js
@@ -5,7 +5,7 @@ const sendEmail = async (to, link) => {
     const transporter = nodemailer.createTransport({
       host: process.env.SMTP_HOST,
       port: process.env.SMTP_PORT,
-      secure: false,
+      secure: true,
       auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASSWORD,
